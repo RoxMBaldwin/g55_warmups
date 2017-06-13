5e40affb250bc682e853adab29c478b906428a84
Roxanne Baldwin <space.legs@Roxannes-MacBook-Air.local>
2017-06-13 10:19:34 -0600

    ✅ warmUp completed

--- 01_arrays/arrays.js
+++ 01_arrays/arrays.js
@@ -1,9 +1,20 @@
+//Arrays are a data structure consiting of a collection of elements, each identified
+//by an index.
+//Create a library that you can add arrays to (without push and reverse)
+
+
 function add(array, item) {
-  // YOUR CODE HERE
-}
+  var library = array
+  library = library.concat(item)
+  return library
+  }
 
 function reverse(array) {
-  // YOUR CODE HERE
+  var reverseLibrary = []
+  for (var i = array.length-1; i >= 0; i--){
+    reverseLibrary = reverseLibrary.concat(array[i])
+  }
+  return reverseLibrary
 }
 
 module.exports = {
